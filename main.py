30aab474a3fee62e47ab3a2cbb31313cc47d4961
gaoj <gaoj@fortinet.com>
2021-10-26 16:16:50 -0700

    add naive paw rescue home

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+"""1. Add a View Function for the Home page."""
 @app.route("/")
 def home():
-    return "Welcome to the HomePage!"
+    return "Paws Rescue Center 🐾"
+
+"""2. Add a View Function for the About page."""
+@app.route("/about")
+def about():
+    return """We are a non-profit organization working as an animal rescue center. 
+        We aim to help you connect with the purrfect furbaby for you! 
+        The animals you find at our website are rescue animals which have been rehabilitated. 
+        Our mission is to promote the ideology of "Adopt, don't Shop"! """
 
 @app.route("/<my_name>")
 def greetings(my_name):
