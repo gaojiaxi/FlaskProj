92e04799144beca63ce398b2d843bbaf24f00a1c
gaoj <gaoj@fortinet.com>
2021-10-26 17:48:16 -0700

    use jinja for loop to render home html

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+"""Information regarding the Pets in the System."""
+pets = [
+            {"id": 1, "name": "Nelly", "age": "5 weeks", "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles."},
+            {"id": 2, "name": "Yuki", "age": "8 months", "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
+            {"id": 3, "name": "Basker", "age": "1 year", "bio": "I love barking. But, I love my friends more."},
+            {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."},
+        ]
+
 """1. Add a View Function for the Home page."""
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", pets = pets)
 """2. Add a View Function for the About page."""
 @app.route("/about")
 def about():
